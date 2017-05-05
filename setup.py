ae2008b2763f40e848d9b185e1e2b1302e2b8165
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2017-05-05 10:37:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-haystack',
-    version='2.0.1-dev',
+    version='2.4.1-dev',
     description='Pluggable search for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
